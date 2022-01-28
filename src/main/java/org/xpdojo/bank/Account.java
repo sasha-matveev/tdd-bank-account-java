a82a179de36567039b09b071a69bd4a6283853f6
sasha matveev <dev.alexander.matveev@gmail.com>
2022-01-28 16:09:40 +0300

    add deposit operations

---
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -0,0 +1,14 @@
+package org.xpdojo.bank;
+
+public class Account {
+
+    private int balance = 0;
+
+    public int balance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+}
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,6 +1,5 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,8 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+	public void shouldHasZeroBalanceOnNewAccount() {
+        final Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldDeposit() {
+        final Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
     }
 }
